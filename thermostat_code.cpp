6de42c7a6c846d766010a9302a804fd0880314c1
Charles Sirichoktanasup <charles.sirich@gmail.com>
2025-11-27 19:04:21 -0500

    Added functionality to change the ID to a different device

--- thermostat_code.cpp
+++ thermostat_code.cpp
@@ -11,29 +11,38 @@ String device_id;
 int counter = 0;
 const int ledPinRed = 26;
 const int ledPinGreen = 25;
+const int ledPinBlue = 27;
 const int toggleButtonPin = 12;
+const int renameButtonPin = 13;
 bool deviceOn = false;
+bool renaming = true;
 int togglenState = 0;
+int renameState = 0;
 StaticJsonDocument<200> doc;
 JsonObject object = doc.to<JsonObject>();
 
 void setup() {
   pinMode(ledPinRed, OUTPUT);
   pinMode(ledPinGreen, OUTPUT);
+  pinMode(ledPinBlue, OUTPUT);
   pinMode(toggleButtonPin, INPUT);
+  pinMode(renameButtonPin, INPUT);
+  digitalWrite(ledPinBlue, HIGH);
   Serial.begin(9600);
   delay(5000);
-  Serial.println("Enter device ID: ");
-  while (Serial.available() == 0) {}
-  device_id = Serial.readString();
+  rename();
   dht.begin();
 }
 
 void loop() {
   delay(500);
   toggleState = digitalRead(toggleButtonPin);
+  renameState = digitalRead(renameButtonPin) && !deviceOn;
   if (toggleState == HIGH) {
     deviceOn = !deviceOn;
+  }
+  if (renameState == HIGH) {
+    renaming = !renaming;
   }
   if (deviceOn) {
     digitalWrite(ledPinRed, LOW);
@@ -41,6 +50,12 @@ void loop() {
   } else {
     digitalWrite(ledPinRed, HIGH);
     digitalWrite(ledPinGreen, LOW);
+  }
+  if (renaming) {
+    digitalWrite(ledPinBlue, HIGH);
+    rename();
+  } else {
+    digitalWrite(ledPinBlue, LOW);
   }
   if (counter % 4 == 0 && deviceOn) {
     float humidity = dht.readHumidity();
@@ -59,4 +74,12 @@ void loop() {
   }
   // Increment counter
   counter++;
+}
+
+int rename() {
+  Serial.println("Enter device ID: ");
+  while (Serial.available() == 0) {}
+  device_id = Serial.readString();
+  renaming = false;
+  return 0;
 }
